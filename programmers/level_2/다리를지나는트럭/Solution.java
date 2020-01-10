d089723ab5cb7a347d61ae80a2ac9d22eda5d060
iiaii <jhun9409@naver.com>
2020-01-10 12:54:12 +0900

    탑,프린터 문제 해결

--- programmers/level_2/다리를지나는트럭/Solution.java
+++ programmers/level_2/다리를지나는트럭/Solution.java
@@ -1,63 +1,88 @@
-package programmers.level_2.다리를지나는트럭;
-
 import java.util.*;
 
 class Solution {
+    static Bridge bridge;
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Bridge bridge = new Bridge(weight, bridge_length);
+        bridge = new Bridge(weight, bridge_length);
         Queue<Truck> pending = new LinkedList<>();
         Queue<Truck> moving = new LinkedList<>();
         
-        for (int w : truck_weights) {
-            pending.add(new Truck(0,w));
-        }
+        pending.addAll(addTrucks(truck_weights));
         
         int count = 0;
         do {
-            for(Truck truck : moving) {
-                truck.moveForward();
-            }
-
-            if(!moving.isEmpty() && bridge.doesTruckPassed(moving.peek())) {
-                bridge.exit(moving.remove());
-            }
-            
-            if(!pending.isEmpty() && bridge.isEnterable(pending.peek())) {
-                Truck truck = pending.remove();
-                truck.moveForward();
-                bridge.enter(truck);
-                moving.add(truck);
-            }
+            moveTrucks(moving);
+            checkTruckPassed(moving);
+            movePendingTruck(pending, moving);
     
             count++;
         } while(!moving.isEmpty());
 
         return count;
     }
+
+    Queue<Truck> addTrucks(int[] truck_weights) {
+        Queue<Truck> pending = new LinkedList<>();
+        
+        for (int w : truck_weights) {
+            pending.add(new Truck(0,w));
+        }
+
+        return pending;
+    }
+
+    void moveTrucks(Queue<Truck> moving) {
+        for(Truck truck : moving) {
+            truck.moveForward();
+        }
+    }
+
+    void checkTruckPassed(Queue<Truck> moving) {
+        if(moving.isEmpty()) {
+            return;
+        }
+        
+        if(bridge.isPassed(moving.peek())) {
+            bridge.exit(moving.remove());
+        }
+    }
+
+    void movePendingTruck(Queue<Truck> pending, Queue<Truck> moving) {
+        if(pending.isEmpty()) {
+            return;
+        }
+
+        if(bridge.isEnterable(pending.peek())) {
+            Truck truck = pending.remove();
+            truck.moveForward();
+            bridge.enter(truck);
+            moving.add(truck);
+        }
+    }
 }
 
 class Bridge {
-    private int maximum_load;
+    private int max_load;
     private int bridge_length;
 
-    Bridge(int maximum_load, int bridge_length) {
-        this.maximum_load = maximum_load;
+    Bridge(int max_load, int bridge_length) {
+        this.max_load = max_load;
         this.bridge_length = bridge_length;
     }
 
     boolean isEnterable(Truck truck) {
-        return this.maximum_load-truck.getWeight() >= 0 ? true : false;
+        return this.max_load-truck.getWeight() >= 0 ? true : false;
     }
 
     void enter(Truck truck) {
-        this.maximum_load -= truck.getWeight();
+        this.max_load -= truck.getWeight();
     }
 
     void exit(Truck truck) {
-        this.maximum_load += truck.getWeight();
+        this.max_load += truck.getWeight();
     }
 
-    boolean doesTruckPassed(Truck truck) {
+    boolean isPassed(Truck truck) {
         return truck.getPosition() > bridge_length;
     }
 }
